e08af622f4e7f1a17c934f3490e2c1d690331b4b
DoubleWoodH <3155965489@qq.com>
2018-03-06 14:01:30 +0800

    add denpencies - react-native-arkit, react-native-spruce

--- Xianyu/android/app/src/main/java/com/xianyu/MainApplication.java
+++ Xianyu/android/app/src/main/java/com/xianyu/MainApplication.java
@@ -3,6 +3,7 @@ package com.xianyu;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import ui.spruce.RNSprucePackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.zmxv.RNSound.RNSoundPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSprucePackage(),
             new ReactVideoPackage(),
             new ReactNativeContacts(),
             new RNSoundPackage(),
